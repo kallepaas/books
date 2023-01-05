a973c2083d0aecf958f35dde726206dfa793d4fe
Kalle <kalle.paas@voco.ee>
2023-01-05 14:17:20 +0200

    Delete single book from book table

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ form.addEventListener('submit', addBook)
 
 document.addEventListener('DOMContentLoaded', getBooks)
 
+const booksTable = document.querySelector('#books')
+booksTable.addEventListener('click', deleteBook)
+
 function getBooks(){
     let books // array for LS data
     if(localStorage.getItem('books') === null){
@@ -13,13 +16,39 @@ function getBooks(){
     books.forEach(function(book){
         const bookRow = document.createElement('tr')
         bookRow.innerHTML = `
-                        <td>${title}</td>
-                        <td>${author}</td>
-                        <td>${isbn}</td>
+                        <td>${book[0]}</td>
+                        <td>${book[1]}</td>
+                        <td>${book[2]}</td>
                         <td><a href="#">X</a></td>`
         const booksTable = document.querySelector('#books')
         booksTable.appendChild(bookRow)
     })
+}
+
+function deleteBook(event){
+    if(event.target.textContent === 'X'){
+        if(confirm('Do you want to delete this book?')){
+            event.target.parentElement.parentElement.remove()
+            const isbn = event.target.parentElement.previousElementSibling
+            const author = event.target.parentElement.previousElementSibling.previousElementSibling
+            const title = event.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling
+
+            const book = [title.textContent, author.textContent, isbn.textContent]
+
+            let books // array for LS data
+            if(localStorage.getItem('books') === null){
+                books = []
+            } else {
+                books = JSON.parse(localStorage.getItem('books'))
+            }
+            books.forEach(function(bookFromLS, index){
+                if( bookFromLS[0] === book[0] && bookFromLS[1] === book[1] && bookFromLS[2] === book[2]){
+                    books.splice(index, 1)
+                }
+            })
+            localStorage.setItem('books', JSON.stringify(books))
+        }
+    }
 }
 
 function addBook(event){
